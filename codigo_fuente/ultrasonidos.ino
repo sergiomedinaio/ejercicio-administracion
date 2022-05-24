8c2c5ead3ea0f52d69ac14657f034a095b1e8e94
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2022-05-24 15:01:15 -0300

    feat: agregar distancia en cm a obstaculo

--- codigo_fuente/ultrasonidos.ino
+++ codigo_fuente/ultrasonidos.ino
@@ -1,3 +1,19 @@
+int ping(int TriggerPin, int EchoPin) {
+    long duration, distanceCm;
+
+    digitalWrite(TriggerPin, LOW);  //para generar un pulso limpio ponemos a LOW 4us
+    delayMicroseconds(4);
+    digitalWrite(TriggerPin, HIGH);  //generamos Trigger (disparo) de 10us
+    delayMicroseconds(10);
+    digitalWrite(TriggerPin, LOW);
+
+    duration = pulseIn(EchoPin, HIGH);  //medimos el tiempo entre pulsos, en microsegundos
+
+    distanceCm = duration * 10 / 292/ 2;   //convertimos a distancia, en cm
+    return distanceCm;
+
+}
+
 void setup() {
     Serial.begin(9600);
     pinMode(TriggerPin, OUTPUT);
