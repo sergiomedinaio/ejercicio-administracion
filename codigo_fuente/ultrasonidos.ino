1ecc4f86cdd39bf1d34c016090105749b6dafbec
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2022-05-24 14:57:51 -0300

    feat: agregar configuracion de los pines

---
+++ codigo_fuente/ultrasonidos.ino
@@ -0,0 +1,9 @@
+void setup() {
+    Serial.begin(9600);
+    pinMode(TriggerPin, OUTPUT);
+    pinMode(EchoPin, INPUT);
+    pinMode(ledPin, OUTPUT);  // declare LED as output
+}
+void loop() {
+
+}
